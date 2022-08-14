e984c54dd86a34930ea4076dd7ed115fc92c6502
Bond <shrekuu@gmail.com>
2022-08-14 21:49:53 +0800

    update homepage content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import siteMetadata from '@generated/site-metadata';
-
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 
 export default function Index(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={siteConfig.title}
-      description={siteConfig.tagline}>
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
       <main>
-        
         <div className="hero shadow--lw bg-[url('/img/index/cheers.webp')] h-20">
           <div className="container">
-            <h1 className="hero__title">交流分享前端开发技术~</h1>
-            <p className="hero__subtitle">点击上面的链接看看吧~</p>
-            <img src="img/index/cheers.webp" alt="干杯" className='block w-80 h-80' />
+            <h1 className="hero__title">Bond 的在线笔记本</h1>
             <div>
-              <button className="button button--secondary button--outline button--lg">
-                Get Started
-              </button>
+              <iframe
+                width="1200"
+                height="675"
+                src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
             </div>
+            <br />
+            <p className="hero__subtitle">吹牛 QQ 群: 343243212</p>
+            <p className="hero__subtitle">Vue 交流 QQ 群: 709382641</p>
           </div>
         </div>
       </main>
